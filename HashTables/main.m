06388bb253d1d0ae8f317a249f0631b0111a1f1d
fatpandaboy <slam19@deerfield.edu>
2019-02-21 14:22:06 -0500

    Initial Commit

---
+++ HashTables/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HashTables
+//
+//  Created by Simon Lam on 2/21/19.
+//  Copyright © 2019 Simon Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
