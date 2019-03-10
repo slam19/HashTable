4bb2c833eb773132044c8f07d0ee4fb7ee109bc9
fatpandaboy <slam19@deerfield.edu>
2019-03-10 12:50:25 -0700

    first commit

---
+++ HashTables/HashTable.h
@@ -0,0 +1,13 @@
+//
+//  HashTable.h
+//  HashTables
+//
+//  Created by Simon Lam on 2/21/19.
+//  Copyright © 2019 Simon Lam. All rights reserved.
+//
+
+#ifndef HashTable_h
+#define HashTable_h
+
+
+#endif /* HashTable_h */
---
+++ HashTables/HashTable.m
@@ -0,0 +1,9 @@
+//
+//  HashTable.m
+//  HashTables
+//
+//  Created by Simon Lam on 2/21/19.
+//  Copyright © 2019 Simon Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
